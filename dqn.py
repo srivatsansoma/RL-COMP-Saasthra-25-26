cbfcfc534a08045d6f67c453e60f2e417742b3d3
SrivatsanS <225315069+srivatsansoma@users.noreply.github.com>
2025-12-03 10:02:26 +0530

    made error corrections in dqn.py

--- dqn.py
+++ dqn.py
@@ -5,17 +5,21 @@ import torch.nn.functional as F
 class dqn(nn.Module):
     def __init__(self, input_dim, output_dim, hidden_dim = [32,32]):
         super(dqn, self).__init__()
-        self.ll2 = nn.Linear(input_dim, output_dim)
-        self.hidden_layers = []
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         
-        prev_dim = input_dim
-        for h_dim in hidden_dim:
-            self.hidden_layers.append(nn.Linear(prev_dim, h_dim))
-            prev_dim = h_dim
-        self.model = nn.Sequential(self.ll2, *self.hidden_layers)
+        layers = []
+        for i in range(len(hidden_dim)):
+            if i == 0:
+                layers.append(nn.Linear(input_dim, hidden_dim[i]))
+            else:
+                layers.append(nn.Linear(hidden_dim[i-1], hidden_dim[i]))
+            layers.append(nn.ReLU())
+
+        layers.append(nn.Linear(hidden_dim[-1], output_dim))
+        self.layers = nn.Sequential(*layers).to(self.device)
 
     def forward(self, x):
-        return self.model(x)
+        return self.layers(x)
     
 if __name__ == "__main__":
     model = dqn(2,2)
